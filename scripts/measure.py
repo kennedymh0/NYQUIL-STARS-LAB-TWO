5c39fffb0870696d7af82c87903dce784615ea9c
Kennedy Haynes <kmhaynes@berkeley.edu>
2026-02-17 21:25:07 -0800

    second commit

---
+++ scripts/measure.py
@@ -0,0 +1,15 @@
+'''
+Authors: Joseph Moreno, Kaitlyn Le, Kennedy Haynes, Helena Insko
+
+Description: Python script developed to measure a radio frequency from the Big Horn on top of Campbell Hall.
+'''
+
+import numpy as np
+from ugradio import sdr
+
+def main():
+    print("Hello! Ready to take a measurement?")
+    
+main()
+    
+    
